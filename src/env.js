314e2eeb7ebd8cb42158892ccca06fbb40a20b8e
David Sanchez <dei@Davids-iMac.local>
2021-03-23 12:29:38 -0500

    https

--- src/env.js
+++ src/env.js
@@ -1,6 +1,7 @@
 const env = {
-    backendPort: 8000,
-    backendURL: '18.234.176.246',
+    backendPort: 8080,
+    //backendURL: '18.234.176.246',
+    backendURL: '54.227.95.228',
     //backendURL: 'localhost',
     prefix: 'https'
 };
